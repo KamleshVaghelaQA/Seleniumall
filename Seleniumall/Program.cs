af39cdf61de86c1a7dd3b6537ae5bbe018ec7f80
OM <OM@DESKTOP-6DDBNNT>
2024-04-26 16:27:19 +0530

    Add project files.

---
+++ Seleniumall/Program.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.DevTools.V122.Console;
+
+namespace Seleniumall
+{
+    class Program
+    {
+        //Webdriver initlize 
+        IWebDriver driver = new ChromeDriver();
+        public static void Main(String[] args)
+        {
+        }
+
+        [SetUp]
+        //Method is use for openbrowser
+        public void OpenBrowser()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Console.WriteLine("Chrome browser open successfully");
+        }
+
+        [Test]
+        //Method is use for perform all operation 
+        public void Function1()
+        {
+
+            //Search Text
+            IWebElement element = driver.FindElement(By.Id("APjFqb"));
+            Seleniumallmethods.Entersampletext(element, "dummy sites for automation testing");
+            OpenQA.Selenium.Interactions.Actions actions = new OpenQA.Selenium.Interactions.Actions(driver);
+            actions.SendKeys(Keys.Enter).Build().Perform();
+            Console.WriteLine("Search Success");
+
+            //Open Search Link
+            IWebElement lnk = driver.FindElement(By.XPath("//*[text()='Demo Testing Site']"));
+            Seleniumallmethods.Click(lnk);
+            Console.WriteLine("Register Link open successfull");
+
+            //Open Sample Link
+            IWebElement sample = driver.FindElement(By.XPath("//*[text()='SamplePage']"));
+            Seleniumallmethods.Click(sample);
+            Console.WriteLine("Sample page Link open successfull");
+
+         /*   //Scrolldown screen 
+            Seleniumallmethods.Scrollscreen(driver);
+            Console.WriteLine("Page scroll successfull");
+         */
+
+
+        }
+
+     
+        [TearDown]
+        public void Stop()
+        {
+            //driver.Quit();
+            Console.WriteLine("Chrome browser close successfully");
+
+        }
+
+
+    }
+}
---
+++ Seleniumall/Seleniumallmethods.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seleniumall
+{
+
+    class Seleniumallmethods
+    {
+       
+
+        public static void Entersampletext(IWebElement element, String text)
+        {
+            element.Clear();
+            element.SendKeys(text);
+
+        }
+
+        public static void Click(IWebElement element)
+        {
+            element.Click();
+
+        }
+
+        public static void Scrollscreen(IWebDriver driver)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollTo(0,1500);");
+        }
+
+
+
+      
+    }
+}
